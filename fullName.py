811e5ad90c8182e61653c721260808c147c09cf4
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2020-09-13 14:30:53 -0400

    Update fullName.py

--- fullName.py
+++ fullName.py
@@ -1,4 +1,3 @@
-# fullName.py - Lab2
 # Author: Daeshaun Morrison
 # Date: 9/1/2020
 
@@ -8,25 +7,36 @@
 continueNaming = True
 # While continueNaming is true...ran the program
 while continueNaming :
+        
     #User inputs their first & last name
     firstName = input("What's your first name?")
-    lastName = input("What's your last name(s)?")
+    lastName = input("What's your last name(s)?")   
     # logs it
     print(f"{lastName}, {firstName}")
-    
+
     # Ask if they want to continue
-    noOrYes = input(f"Do you want to continue? (Y/N)")
-    
+    while True:
+        noOrYes = input(f"Do you want to continue? (Y/N)")
+        if noOrYes.lower().startswith('y'):
+            #User gave a proper response
+            #we're ready to exit the loop.
+            break
+        elif noOrYes.lower().startswith('n'):
+            #User gave a proper response,
+            #we're ready to exit the loop.
+            break
+        else: 
+            print("Sorry, I didn't understand that.")
+            #Better try again... returning to the start of the loop
+            continue
+        
     # If yes or any other programmed equivalent, "continueNaming = True",
     # which contines the program
     if noOrYes.lower().startswith('y'):
-        continueNaming = True
-        
+            continueNaming = True
+            
     # If no or any other programmed equivalent, "continueNaming = false",
     # ending the program   
     elif noOrYes.lower().startswith('n'):
         continueNaming = False
-        
-    elif noOrYes != "Y" or "N":
-        noOrYes = input(f"That's not an answer, do you want to continue? (Y/N)")
-    #else: noOrYes = input(f"That's not answer, do you want to continue? (Y/N)")
+            
